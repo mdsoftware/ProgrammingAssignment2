a77a2b71291e7fa1d7d4ea675a7320a1a088f4d7
mdsoftware <mdsoftware@users.noreply.github.com>
2014-04-23 12:10:54 +0400

    Functions implemented

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
-
+##
+## makeCacheMatrix creates a special object using closures as internal data and defined functions as methods.
+## Result is an object which can store value of type matrix and its inversed form.
+## NOTE: Its better to calculate and cache in the getSolve function since this value depends only from
+## the matrix and must not be set from outside.
+##
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() x
+    getSolve <- function(...) {
+        if(is.null(x)){
+            NULL
+        } else if (is.null(inv)) {
+            inv <<- solve(x, ...)   
+            inv
+        } else {
+            inv
+        }
+    }
+    list(set = set, get = get,
+         getSolve = getSolve)
 }
 
 
-## Write a short comment describing this function
+## Return cached inverse or recalculate if needed passing ... to solve function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    x$getSolve(...)
 }
